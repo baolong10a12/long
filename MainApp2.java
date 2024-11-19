1d5af76238c29f8d71b1296b5b2631b9cefa6205
baolong10a12 <nguyentomvn247@gmail.com>
2024-11-19 10:52:08 +0700

    Add files via upload

---
+++ MainApp2.java
@@ -0,0 +1,28 @@
+package basic.dev;
+
+public class MainApp2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// thong tin ca nhan
+         String ten = "bao long";
+         String msv = "08492874";
+         String cmnd = "0392849489";
+         String lop = "10a12";
+         String truong = "Hueic";
+         Double dtb = 6.5;
+         
+         // thong tin cong nhan
+         String t = "bao long";
+         String dc = "hue";
+         String sdt = "0857573825";
+         Double hsl = 7.920938;
+         
+         // thong tin xe
+         String xe = "mercedes";
+         String h = "duc";
+         Double gia = 9.8;
+         String m = "white";
+	}
+
+}
