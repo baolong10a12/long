89ab2ec288129f3deff95e16e9a663bce45b45ab
baolong10a12 <nguyentomvn247@gmail.com>
2024-12-10 10:33:02 +0700

    Add files via upload

---
+++ IOjava/src/basic/dev/MainApp.java
@@ -0,0 +1,73 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// THONG TIN SINH VIEN
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ten sv");
+		String tenSv = sc.nextLine();
+		System.out.println("Ten" + tenSv);
+		
+
+
+		System.out.println("So dien thoai");
+		String phoneSv = sc.nextLine();
+		System.out.println("Phone:" + phoneSv);
+		
+		System.out.println("Ma sinh vien");
+		String codeSv = sc.nextLine();
+		System.out.println("code:" + codeSv);
+		
+		System.out.println("ten lop");
+		String classsv = sc.nextLine();
+		System.out.println("class:" + classsv);
+		
+		System.out.println("ten truong");
+		String schoolsv = sc.nextLine();
+		System.out.println("school:" + schoolsv);
+		
+		System.out.println("dien trung binh");
+		String avergare = sc.nextLine();
+		System.out.println("code:" + codeSv);
+		System.out.println("0094827: ");
+		System.out.println("nguyen quoc bao long: ");
+		System.out.println("094728: ");
+		System.out.println("8656985456: ");
+		System.out.println("10a12: ");
+		System.out.println("Cao Dang Cong Nghiep HUEIC: ");
+		System.out.println("6.5: ");
+		// THONG TIN CONG NHAN
+		System.out.println("nguyen quoc bao long: ");
+		System.out.println("131 tran phu: ");
+		System.out.println("0398392638: ");
+		System.out.println("100000: ");
+		// THONG TIN CHIEC XE
+		System.out.println("mesceder e300: ");
+		System.out.println("đức: ");
+		System.out.println("2099000000vnd: ");
+		System.out.println("black: ");
+		// THONG TIN SINH VIEN
+		System.out.println("0094827: ");
+		System.out.println("nguyen quoc bao long: ");
+		System.out.println("094728: ");
+		System.out.println("8656985456: ");
+		System.out.println("10a12: ");
+		System.out.println("Cao Dang Cong Nghiep HUEIC: ");
+		System.out.println("6.5: ");
+		// THONG TIN CONG NHAN
+		System.out.println("nguyen quoc bao long: ");
+		System.out.println("131 tran phu: ");
+		System.out.println("0398392638: ");
+		System.out.println("100000: ");
+		// THONG TIN CHIEC XE
+		System.out.println("mesceder e300: ");
+		System.out.println("đức: ");
+		System.out.println("2099000000vnd: ");
+		System.out.println("black: ");
+	}
+
+}
